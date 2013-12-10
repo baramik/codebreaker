8eb217ba68790e6064f86dca3860d39487e41497
baramik <baramik1989@gmail.com>
2013-12-10 15:40:12 +0200

    Update codegenerator_spec.rb

--- spec/codegenerator_spec.rb
+++ spec/codegenerator_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 require "codebreaker/codegenerator"
 
 describe Codebreaker::Code_generator do 
-	let(:new_generator){Codebreaker::Code_generator.new}
-	context ".new" do 
-		it "instatiate a new Code_generator with empty string" do 
-			expect(new_generator.random_string).to be_empty
-		end		
-	end
+  let(:new_generator){Codebreaker::Code_generator.new}
+  context ".new" do 
+    it "instatiate a new Code_generator with empty string" do 
+      expect(new_generator.random_string).to be_empty
+    end   
+  end
 
-	context "#random" do
-		it "creates a new random string" do
-			expect(new_generator.random) == 4
-		end
-	end
+  context "#random" do
+    it "creates a new random string" do
+      expect(new_generator.random) == 4
+    end
+  end
 end
