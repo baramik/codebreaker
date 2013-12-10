1ae92fa2879044272d5d4916e0a3eac958eaf0f5
baramik <baramik1989@gmail.com>
2013-12-10 13:54:02 +0200

    Update storage.rb

--- lib/codebreaker/storage.rb
+++ lib/codebreaker/storage.rb
@@ -1,7 +1,7 @@
 module Codebreaker
   class Storage
     def save_results
-      File.open(f_name, 'w') { |file| file.write(" Player: #{@player.name} finished game.\n Number of attempts: #{@number_of_attempts}") }	
+      File.open(f_name, 'w') { |file| file.write(" Player: #{@player.user_name} finished game.\n Number of attempts: #{@attempts_number}") }	
     end
   end
 end
